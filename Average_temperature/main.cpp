ad6a276c8347e9ef4d53f2b5767d84316eb52c44
Serhii Olianovskyi <57589024+olanser@users.noreply.github.com>
2020-09-07 16:28:13 +0300

    Add Average_temperature

---
+++ Average_temperature/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int32_t numberDays;
+    vector<int32_t> temperatureDays;
+    int32_t temperature;
+    int64_t sum = 0;
+    cin >> numberDays;
+    while (numberDays-- > 0) {
+        cin >> temperature;
+        temperatureDays.push_back(temperature);
+        sum += temperature;
+    }
+    sum /= static_cast<int64_t>(temperatureDays.size());
+    int32_t countTemp = 0;
+    for (auto temp : temperatureDays) {
+        if (temp > sum)
+            ++countTemp;
+    }
+    cout << countTemp << endl;
+    for (size_t i = 0; i < temperatureDays.size(); i++) {
+        if (temperatureDays[i] > sum) {
+            cout << i;
+            if (--countTemp == 0)
+                cout << endl;
+            else
+                cout << " ";
+        }
+    }
+}
